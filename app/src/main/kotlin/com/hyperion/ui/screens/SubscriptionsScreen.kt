6b114b48281abb7e124eb598ba867c928602d38a
zt64 <nwowens32@gmail.com>
2022-03-20 18:13:03 -0400

    Added placeholders to Subscriptions and Library screen

--- app/src/main/kotlin/com/hyperion/ui/screens/SubscriptionsScreen.kt
+++ app/src/main/kotlin/com/hyperion/ui/screens/SubscriptionsScreen.kt
@@ -1,10 +1,32 @@
 package com.hyperion.ui.screens
 
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Construction
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import com.ramcosta.composedestinations.annotation.Destination
 
 @Destination
 @Composable
 fun SubscriptionsScreen() {
-    // TODO
+    Box(
+        modifier = Modifier.fillMaxSize()
+    ) {
+        Column(
+            modifier = Modifier.align(Alignment.Center),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Icon(
+                imageVector = Icons.Default.Construction,
+                contentDescription = null
+            )
+            Text("Incomplete")
+        }
+    }
 }
