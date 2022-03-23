1f65fd3ef1964a0474b157b9f90fd6b29aa6cd6a
zt64 <nwowens32@gmail.com>
2022-03-23 16:51:45 -0400

    Redesigned intro screen

--- app/src/main/kotlin/com/hyperion/ui/screens/IntroScreen.kt
+++ app/src/main/kotlin/com/hyperion/ui/screens/IntroScreen.kt
@@ -1,10 +1,16 @@
 package com.hyperion.ui.screens
 
-import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.*
 import androidx.compose.material3.Button
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import com.hyperion.R
 import com.hyperion.preferences.Prefs
 import com.ramcosta.composedestinations.annotation.Destination
 
@@ -15,10 +21,39 @@ import com.ramcosta.composedestinations.annotation.Destination
 @Composable
 fun IntroScreen() {
     Column(
-        horizontalAlignment = Alignment.CenterHorizontally
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(horizontal = 14.dp, vertical = 8.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
     ) {
-        Button(onClick = { Prefs.firstLaunch = false }) {
-            Text("Ok")
+        Icon(
+            painter = painterResource(R.drawable.ic_launcher_foreground),
+            contentDescription = null
+        )
+
+        Text(
+            text = "Thanks for installing Hyperion! Currently only the basic features are implemented, but more are sure to come!",
+            style = MaterialTheme.typography.bodyLarge
+        )
+
+        Column(
+            modifier = Modifier.width(IntrinsicSize.Max)
+        ) {
+            Button(
+                modifier = Modifier.fillMaxWidth(),
+                enabled = false,
+                onClick = { /*TODO*/ }
+            ) {
+                Text("Login to Google")
+            }
+
+            Button(
+                modifier = Modifier.fillMaxWidth(),
+                onClick = { Prefs.firstLaunch = false }
+            ) {
+                Text("Continue without login")
+            }
         }
     }
 }
