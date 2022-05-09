7c2a8c5ff676b11bea36675faba378117e47c3c5
zt64 <nwowens32@gmail.com>
2022-05-09 13:29:20 -0400

    Update gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,7 +6,7 @@ buildscript {
     }
 
     dependencies {
-        classpath("com.android.tools.build:gradle:7.1.2")
+        classpath("com.android.tools.build:gradle:7.1.3")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.6.10")
         classpath("com.google.dagger:hilt-android-gradle-plugin:2.38.1")
         // NOTE: Do not place your application dependencies here; they belong
