e457874e818ee598ac64fa34b111e3a8d040a751
zt64 <nwowens32@gmail.com>
2022-05-13 14:44:18 -0400

    Fix VideoCard thumbnail

--- app/src/main/kotlin/com/hyperion/ui/components/VideoCard.kt
+++ app/src/main/kotlin/com/hyperion/ui/components/VideoCard.kt
@@ -4,7 +4,6 @@ import android.annotation.SuppressLint
 import android.content.res.Configuration
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.ElevatedCard
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
@@ -12,14 +11,11 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.ExperimentalUnitApi
-import androidx.compose.ui.unit.TextUnit
-import androidx.compose.ui.unit.TextUnitType
 import androidx.compose.ui.unit.dp
 import coil.compose.AsyncImage
 import coil.request.ImageRequest
@@ -46,7 +42,8 @@ fun VideoCard(
                             .data(video.thumbnailUrl)
                             .crossfade(true)
                             .build(),
-                        contentDescription = stringResource(R.string.thumbnail)
+                        contentDescription = stringResource(R.string.thumbnail),
+                        contentScale = ContentScale.Crop
                     )
 
                     if (video.timestamp != null) {
@@ -93,12 +90,13 @@ fun VideoCard(
                         modifier = Modifier.fillMaxHeight()
                     ) {
                         AsyncImage(
-                            modifier = Modifier.fillMaxHeight(),
+                            modifier = Modifier.aspectRatio(16f / 9f),
                             model = ImageRequest.Builder(LocalContext.current)
                                 .data(video.thumbnailUrl)
                                 .crossfade(true)
                                 .build(),
-                            contentDescription = stringResource(R.string.thumbnail)
+                            contentDescription = stringResource(R.string.thumbnail),
+                            contentScale = ContentScale.Crop
                         )
 
                         if (video.timestamp != null) {
