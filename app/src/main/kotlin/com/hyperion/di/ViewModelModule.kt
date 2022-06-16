322f6f2b119941624db5034ef0e1269165cc2d69
zt64 <nwowens32@gmail.com>
2022-06-16 19:14:44 -0400

    Fix crash when navigating to a channel

--- app/src/main/kotlin/com/hyperion/di/ViewModelModule.kt
+++ app/src/main/kotlin/com/hyperion/di/ViewModelModule.kt
@@ -1,15 +1,13 @@
 package com.hyperion.di
 
-import com.hyperion.ui.viewmodel.MainViewModel
-import com.hyperion.ui.viewmodel.PlayerViewModel
-import com.hyperion.ui.viewmodel.SearchViewModel
-import com.hyperion.ui.viewmodel.SettingsViewModel
+import com.hyperion.ui.viewmodel.*
 import org.koin.androidx.viewmodel.dsl.viewModelOf
 import org.koin.dsl.module
 
 val viewModelModule = module {
     viewModelOf(::MainViewModel)
     viewModelOf(::PlayerViewModel)
+    viewModelOf(::ChannelViewModel)
     viewModelOf(::SearchViewModel)
     viewModelOf(::SettingsViewModel)
 }
