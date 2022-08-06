aae5ddf57995768b23bf86750b7d55b9f5ad3bc7
zt <nwowens32@gmail.com>
2022-08-06 19:25:44 -0400

    Update gradle and Android gradle plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,7 +6,7 @@ buildscript {
     }
 
     dependencies {
-        classpath("com.android.tools.build:gradle:7.4.0-alpha08")
+        classpath("com.android.tools.build:gradle:7.4.0-alpha09")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.7.10")
         // NOTE: Do not place your application dependencies here; they belong
         // in the individual module build.gradle.kts files
