a133e254691b48eb4880ab550e9a184084742430
zt64 <nwowens32@gmail.com>
2022-09-30 12:51:21 -0400

    Update ktor

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -94,7 +94,7 @@ dependencies {
     implementation("androidx.media3:media3-ui:$media3Version")
 
     // Ktor
-    val ktorVersion = "2.1.1"
+    val ktorVersion = "2.1.2"
     implementation("io.ktor:ktor-client-core:$ktorVersion")
     implementation("io.ktor:ktor-client-cio:$ktorVersion")
     implementation("io.ktor:ktor-client-android:$ktorVersion")
