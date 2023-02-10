581f3daa83af86391e90ee7acf213e3d199ab8a2
zt64 <nwowens32@gmail.com>
2023-02-09 23:13:46 -0500

    feat: add placeholder drawer

--- app/src/main/kotlin/com/hyperion/ui/navigation/BaseScreen.kt
+++ app/src/main/kotlin/com/hyperion/ui/navigation/BaseScreen.kt
@@ -3,6 +3,7 @@ package com.hyperion.ui.navigation
 import androidx.compose.animation.ExperimentalAnimationApi
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Menu
 import androidx.compose.material.icons.filled.Search
 import androidx.compose.material.icons.filled.Settings
 import androidx.compose.material3.*
@@ -10,11 +11,14 @@ import androidx.compose.material3.windowsizeclass.WindowHeightSizeClass
 import androidx.compose.material3.windowsizeclass.WindowSizeClass
 import androidx.compose.material3.windowsizeclass.WindowWidthSizeClass
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
 import com.hyperion.R
 import dev.olshevski.navigation.reimagined.*
+import kotlinx.coroutines.launch
 
 private fun <T> NavController<T>.switchTo(destination: T) {
     if (!moveToTop { it == destination }) navigate(destination)
@@ -31,80 +35,110 @@ fun BaseScreen(
 ) {
     val navController = rememberNavController(BaseDestination.HOME)
     val scrollBehavior = TopAppBarDefaults.exitUntilCollapsedScrollBehavior()
+    val drawerState = rememberDrawerState(DrawerValue.Closed)
 
-    Scaffold(
-        modifier = Modifier.nestedScroll(scrollBehavior.nestedScrollConnection),
-        topBar = {
-            MediumTopAppBar(
-                title = {
-                    Text(stringResource(navController.currentDestination.label))
-                },
-                actions = {
-                    IconButton(onClick = onClickSearch) {
-                        Icon(
-                            imageVector = Icons.Default.Search,
-                            contentDescription = stringResource(R.string.search)
-                        )
-                    }
-                    IconButton(onClick = onClickSettings) {
-                        Icon(
-                            imageVector = Icons.Default.Settings,
-                            contentDescription = stringResource(R.string.settings)
-                        )
-                    }
-                },
-                scrollBehavior = scrollBehavior
-            )
-        },
-        bottomBar = {
-            if (
-                windowSizeClass.widthSizeClass == WindowWidthSizeClass.Compact &&
-                windowSizeClass.heightSizeClass != WindowHeightSizeClass.Compact
-            ) {
-                NavigationBar {
-                    BaseDestination.values().forEach { destination ->
-                        NavigationBarItem(
-                            selected = navController.currentDestination == destination,
-                            icon = { Icon(destination.icon, null) },
-                            label = if (!hideNavLabel) {
-                                { Text(stringResource(destination.label)) }
-                            } else null,
-                            onClick = { navController.switchTo(destination) }
-                        )
-                    }
-                }
+    ModalNavigationDrawer(
+        drawerState = drawerState,
+        drawerContent = {
+            ModalDrawerSheet {
+                Text(
+                    text = stringResource(R.string.app_name),
+                    style = MaterialTheme.typography.titleLarge,
+                    modifier = Modifier.padding(16.dp)
+                )
             }
         }
-    ) { paddingValues ->
-        Row(
-            modifier = Modifier
-                .fillMaxSize()
-                .padding(paddingValues)
-        ) {
-            if (windowSizeClass.widthSizeClass != WindowWidthSizeClass.Compact) {
-                NavigationRail {
-                    BaseDestination.values().forEach { destination ->
-                        NavigationRailItem(
-                            selected = navController.currentDestination == destination,
-                            icon = { Icon(destination.icon, null) },
-                            label = if (!hideNavLabel) {
-                                { Text(stringResource(destination.label)) }
-                            } else null,
-                            onClick = { navController.switchTo(destination) }
-                        )
+    ) {
+        Scaffold(
+            modifier = Modifier.nestedScroll(scrollBehavior.nestedScrollConnection),
+            topBar = {
+                MediumTopAppBar(
+                    title = {
+                        Text(stringResource(navController.currentDestination.label))
+                    },
+                    navigationIcon = {
+                        val coroutineScope = rememberCoroutineScope()
+
+                        IconButton(
+                            onClick = {
+                                coroutineScope.launch {
+                                    drawerState.open()
+                                }
+                            }
+                        ) {
+                            Icon(
+                                imageVector = Icons.Default.Menu,
+                                contentDescription = null
+                            )
+                        }
+                    },
+                    actions = {
+                        IconButton(onClick = onClickSearch) {
+                            Icon(
+                                imageVector = Icons.Default.Search,
+                                contentDescription = stringResource(R.string.search)
+                            )
+                        }
+                        IconButton(onClick = onClickSettings) {
+                            Icon(
+                                imageVector = Icons.Default.Settings,
+                                contentDescription = stringResource(R.string.settings)
+                            )
+                        }
+                    },
+                    scrollBehavior = scrollBehavior
+                )
+            },
+            bottomBar = {
+                if (
+                    windowSizeClass.widthSizeClass == WindowWidthSizeClass.Compact &&
+                    windowSizeClass.heightSizeClass != WindowHeightSizeClass.Compact
+                ) {
+                    NavigationBar {
+                        BaseDestination.values().forEach { destination ->
+                            NavigationBarItem(
+                                selected = navController.currentDestination == destination,
+                                icon = { Icon(destination.icon, null) },
+                                label = if (!hideNavLabel) {
+                                    { Text(stringResource(destination.label)) }
+                                } else null,
+                                onClick = { navController.switchTo(destination) }
+                            )
+                        }
                     }
                 }
             }
-
-            Box(
+        ) { paddingValues ->
+            Row(
                 modifier = Modifier
-                    .weight(1f, true)
-                    .fillMaxHeight()
+                    .fillMaxSize()
+                    .padding(paddingValues)
             ) {
-                AnimatedNavHost(
-                    controller = navController
-                ) { destination ->
-                    content(destination)
+                if (windowSizeClass.widthSizeClass != WindowWidthSizeClass.Compact) {
+                    NavigationRail {
+                        BaseDestination.values().forEach { destination ->
+                            NavigationRailItem(
+                                selected = navController.currentDestination == destination,
+                                icon = { Icon(destination.icon, null) },
+                                label = if (!hideNavLabel) {
+                                    { Text(stringResource(destination.label)) }
+                                } else null,
+                                onClick = { navController.switchTo(destination) }
+                            )
+                        }
+                    }
+                }
+
+                Box(
+                    modifier = Modifier
+                        .weight(1f, true)
+                        .fillMaxHeight()
+                ) {
+                    AnimatedNavHost(
+                        controller = navController
+                    ) { destination ->
+                        content(destination)
+                    }
                 }
             }
         }
