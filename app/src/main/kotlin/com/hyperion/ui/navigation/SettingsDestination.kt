f99978ebec663019432a1eafcc391a21b4d66f0a
zt64 <nwowens32@gmail.com>
2023-05-27 19:51:54 -0400

    feat: add sponsorblock category settings

--- app/src/main/kotlin/com/hyperion/ui/navigation/SettingsDestination.kt
+++ app/src/main/kotlin/com/hyperion/ui/navigation/SettingsDestination.kt
@@ -1,24 +1,18 @@
 package com.hyperion.ui.navigation
 
-import android.os.Parcelable
 import androidx.annotation.StringRes
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.*
 import androidx.compose.ui.graphics.vector.ImageVector
 import com.hyperion.R
-import kotlinx.parcelize.IgnoredOnParcel
-import kotlinx.parcelize.Parcelize
-import kotlinx.parcelize.RawValue
 
-@Parcelize
-sealed interface SettingsDestination : Parcelable {
+sealed interface SettingsDestination {
     @get:StringRes
     val label: Int
 }
 
-@Parcelize
 enum class SettingsSection(
-    val icon: @RawValue ImageVector,
+    val icon: ImageVector,
     @StringRes override val label: Int,
 ) : SettingsDestination {
     GENERAL(Icons.Default.Settings, R.string.general),
@@ -30,9 +24,7 @@ enum class SettingsSection(
     BACKUP_RESTORE(Icons.Default.SettingsBackupRestore, R.string.backup_restore),
     ABOUT(Icons.Default.Info, R.string.about);
 
-    @Parcelize
     companion object : SettingsDestination {
-        @IgnoredOnParcel
         override val label = R.string.settings
     }
 }
--- app/src/main/kotlin/com/hyperion/ui/screen/settings/SponsorBlockScreen.kt
+++ app/src/main/kotlin/com/hyperion/ui/screen/settings/SponsorBlockScreen.kt
@@ -1,8 +1,11 @@
 package com.hyperion.ui.screen.settings
 
 import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.ColumnScope
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ContentCopy
 import androidx.compose.material.icons.filled.Edit
@@ -16,15 +19,20 @@ import androidx.compose.ui.focus.focusRequester
 import androidx.compose.ui.platform.LocalClipboardManager
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.AnnotatedString
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardCapitalization
+import androidx.compose.ui.unit.dp
 import com.hyperion.R
 import com.hyperion.domain.manager.PreferencesManager
+import com.hyperion.ui.component.player.SponsorBlockCategory
 import com.hyperion.ui.component.setting.SliderSetting
 import com.hyperion.ui.component.setting.SwitchSetting
 
 context(ColumnScope)
 @Composable
 fun SponsorBlockScreen(
-    preferences: PreferencesManager
+    preferences: PreferencesManager,
+    onClickCategory: (SponsorBlockCategory) -> Unit
 ) {
     SwitchSetting(
         checked = preferences.sponsorBlockEnabled,
@@ -33,41 +41,95 @@ fun SponsorBlockScreen(
     )
 
     AnimatedVisibility(visible = preferences.sponsorBlockEnabled) {
-        var showUserIdDialog by rememberSaveable { mutableStateOf(false) }
+        Column(
+            verticalArrangement = Arrangement.spacedBy(12.dp)
+        ) {
+            var showUserIdDialog by rememberSaveable { mutableStateOf(false) }
 
-        SliderSetting(
-            text = stringResource(R.string.skip_notice_duration),
-            value = preferences.sponsorBlockSkipNoticeDuration.toFloat(),
-            valueRange = 0f..10f,
-            onValueChange = { preferences.sponsorBlockSkipNoticeDuration = it.toInt() },
-            onValueChangeFinished = { preferences.sponsorBlockSkipNoticeDuration = it.toInt() }
-        )
-
-        if (showUserIdDialog) {
-            UserIdDialog(
-                id = preferences.sponsorBlockUserIdPrivate,
-                onClickSave = { /*TODO*/ },
-                onDismissRequest = { showUserIdDialog = false }
+            SliderSetting(
+                text = stringResource(R.string.skip_notice_duration),
+                value = preferences.sponsorBlockSkipNoticeDuration.toFloat(),
+                valueRange = 0f..10f,
+                onValueChange = { preferences.sponsorBlockSkipNoticeDuration = it.toInt() },
+                onValueChangeFinished = { preferences.sponsorBlockSkipNoticeDuration = it.toInt() }
             )
-        }
 
-        ListItem(
-            modifier = Modifier.clickable { showUserIdDialog = true },
-            headlineContent = {
-                Text("Private UserID")
-            },
-            supportingContent = {
-                Text("**********************")
-            },
-            trailingContent = {
-                IconButton(onClick = { showUserIdDialog = true }) {
-                    Icon(
-                        imageVector = Icons.Default.Edit,
-                        contentDescription = null
-                    )
-                }
+            if (showUserIdDialog) {
+                UserIdDialog(
+                    id = preferences.sponsorBlockUserIdPrivate,
+                    onClickSave = { showUserIdDialog = false },
+                    onDismissRequest = { showUserIdDialog = false }
+                )
             }
-        )
+
+            SwitchSetting(
+                checked = preferences.sponsorBlockSkipTracking,
+                text = stringResource(R.string.skip_count_tracking),
+                onCheckedChange = { preferences.sponsorBlockSkipTracking = it }
+            )
+
+            ListItem(
+                modifier = Modifier.clickable { showUserIdDialog = true },
+                headlineContent = {
+                    Text("Private UserID")
+                },
+                supportingContent = {
+                    Text("**********************")
+                },
+                trailingContent = {
+                    IconButton(onClick = { showUserIdDialog = true }) {
+                        Icon(
+                            imageVector = Icons.Default.Edit,
+                            contentDescription = null
+                        )
+                    }
+                }
+            )
+
+            OutlinedTextField(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(horizontal = 16.dp),
+                value = preferences.sponsorBlockApiUrl,
+                onValueChange = { preferences.sponsorBlockApiUrl = it },
+                singleLine = true,
+                keyboardOptions = KeyboardOptions(
+                    capitalization = KeyboardCapitalization.None,
+                    autoCorrect = false,
+                    imeAction = ImeAction.Done
+                ),
+                label = {
+                    Text("API URL")
+                }
+            )
+
+            Divider(
+                modifier = Modifier.padding(horizontal = 16.dp)
+            )
+
+            val categories = remember(SponsorBlockCategory::values)
+
+            categories.forEach { category ->
+                ListItem(
+                    modifier = Modifier.clickable {
+                        onClickCategory(category)
+                    },
+                    headlineContent = {
+                        Text(category.toString())
+                    },
+                    supportingContent = {
+                        Text(category.description)
+                    },
+                    trailingContent = {
+                        Box(
+                            modifier = Modifier
+                                .size(36.dp)
+                                .background(category.color, CircleShape)
+                        )
+                    }
+                )
+            }
+        }
     }
 }
 
