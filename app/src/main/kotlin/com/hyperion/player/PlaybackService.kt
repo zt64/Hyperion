5790966fe8d7133e6b124d1909b681d8c406a02a
zt64 <nwowens32@gmail.com>
2023-05-31 15:55:00 -0400

    feat: update player configuration

--- app/src/main/kotlin/com/hyperion/player/PlaybackService.kt
+++ app/src/main/kotlin/com/hyperion/player/PlaybackService.kt
@@ -1,16 +1,24 @@
 package com.hyperion.player
 
+import android.app.PendingIntent
+import android.content.Intent
 import androidx.media3.common.AudioAttributes
 import androidx.media3.common.C
 import androidx.media3.common.MediaItem
 import androidx.media3.common.util.UnstableApi
+import androidx.media3.datasource.DefaultHttpDataSource
+import androidx.media3.exoplayer.DefaultLoadControl
 import androidx.media3.exoplayer.DefaultRenderersFactory
 import androidx.media3.exoplayer.ExoPlayer
+import androidx.media3.exoplayer.source.ProgressiveMediaSource
 import androidx.media3.exoplayer.trackselection.DefaultTrackSelector
+import androidx.media3.exoplayer.upstream.DefaultAllocator
 import androidx.media3.session.MediaSession
+import androidx.media3.session.MediaSession.ConnectionResult
 import androidx.media3.session.MediaSessionService
 import com.google.common.util.concurrent.Futures
 import com.google.common.util.concurrent.ListenableFuture
+import com.hyperion.ui.MainActivity
 
 @UnstableApi
 class PlaybackService : MediaSessionService() {
@@ -33,16 +41,47 @@ class PlaybackService : MediaSessionService() {
                 DefaultRenderersFactory(application)
                     .setEnableAudioTrackPlaybackParams(true)
                     .setEnableAudioOffload(true)
+                    .forceEnableMediaCodecAsynchronousQueueing()
+                    .setExtensionRendererMode(DefaultRenderersFactory.EXTENSION_RENDERER_MODE_PREFER)
+                    .setEnableDecoderFallback(true)
+            )
+            .setMediaSourceFactory(ProgressiveMediaSource.Factory(DefaultHttpDataSource.Factory()))
+            .setUseLazyPreparation(true)
+            .setLoadControl(
+                DefaultLoadControl.Builder()
+                    .setBufferDurationsMs(
+                        /* minBufferMs= */ 2000,
+                        /* maxBufferMs= */ 5000,
+                        /* bufferForPlaybackMs= */ 1500,
+                        /* bufferForPlaybackAfterRebufferMs= */ 2000
+                    )
+                    .setPrioritizeTimeOverSizeThresholds(true)
+                    .setTargetBufferBytes(C.LENGTH_UNSET)
+                    .setBackBuffer(
+                        /* backBufferDurationMs= */ 10000,
+                        /* retainBackBufferFromKeyframe= */ false
+                    )
+                    .setAllocator(DefaultAllocator(true, 16))
+                    .build()
             )
             .setWakeMode(C.WAKE_MODE_NETWORK)
             .setHandleAudioBecomingNoisy(true)
             .setSeekBackIncrementMs(15000)
             .setSeekForwardIncrementMs(15000)
+            .setPauseAtEndOfMediaItems(false)
             .setVideoScalingMode(C.VIDEO_SCALING_MODE_SCALE_TO_FIT_WITH_CROPPING)
             .build()
 
         mediaSession = MediaSession.Builder(application, player)
             .setCallback(MediaSessionCallback())
+            .setSessionActivity(
+                PendingIntent.getActivity(
+                    /* context = */ application,
+                    /* requestCode = */ 0,
+                    /* intent = */ Intent(application, MainActivity::class.java),
+                    /* flags = */ PendingIntent.FLAG_IMMUTABLE
+                )
+            )
             .build()
 
         player.playWhenReady = true
@@ -58,12 +97,12 @@ class PlaybackService : MediaSessionService() {
     }
 
     private inner class MediaSessionCallback : MediaSession.Callback {
-        override fun onConnect(session: MediaSession, controller: MediaSession.ControllerInfo): MediaSession.ConnectionResult {
+        override fun onConnect(session: MediaSession, controller: MediaSession.ControllerInfo): ConnectionResult {
             val connectionResult = super.onConnect(session, controller)
             val sessionCommands = connectionResult.availableSessionCommands
             val playerCommands = connectionResult.availablePlayerCommands
 
-            return MediaSession.ConnectionResult.accept(sessionCommands, playerCommands)
+            return ConnectionResult.accept(sessionCommands, playerCommands)
         }
 
         override fun onAddMediaItems(
