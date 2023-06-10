ace27e2c412f7e0587bb02096376efecead5e8ad
zt64 <nwowens32@gmail.com>
2023-06-09 23:55:56 -0400

    fix: remove lag caused by account manager

--- app/src/main/kotlin/com/hyperion/domain/manager/AccountManager.kt
+++ app/src/main/kotlin/com/hyperion/domain/manager/AccountManager.kt
@@ -3,28 +3,23 @@ package com.hyperion.domain.manager
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
-import com.zt.innertube.network.dto.ClientInfo
 import com.zt.innertube.network.service.InnerTubeService
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.runBlocking
 import java.util.UUID
 
 class AccountManager(
-    private val preferencesManager: PreferencesManager,
+    preferencesManager: PreferencesManager,
     private val innerTubeService: InnerTubeService
 ) {
-    private val deviceId: String = UUID.randomUUID().toString()
-    private var clientInfo: ClientInfo
+    private val deviceId: String
 
     var loggedIn: Boolean by mutableStateOf(false)
 
     init {
-        runBlocking(Dispatchers.IO) {
-            clientInfo = innerTubeService.getClientInfo()
-        }
+        deviceId = preferencesManager.deviceId.takeUnless { it.isEmpty() }
+            ?: UUID.randomUUID().toString().also { preferencesManager.deviceId = it }
     }
 
     suspend fun getCode(): String {
-        return innerTubeService.getUserCode(clientInfo.clientId, deviceId).userCode
+        TODO()
     }
 }
