228c9a3a639e0dca450b7057b7454d814fe401ec
zt64 <nwowens32@gmail.com>
2023-09-02 13:47:22 -0400

    fix image loading and error

--- common/src/commonMain/kotlin/dev/zt64/hyperion/ui/component/ShimmerImage.kt
+++ common/src/commonMain/kotlin/dev/zt64/hyperion/ui/component/ShimmerImage.kt
@@ -1,11 +1,17 @@
 package dev.zt64.hyperion.ui.component
 
+import androidx.compose.animation.Crossfade
+import androidx.compose.animation.core.tween
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.ColorFilter
+import androidx.compose.ui.graphics.DefaultAlpha
+import androidx.compose.ui.graphics.FilterQuality
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.unit.dp
 import com.seiko.imageloader.*
@@ -15,13 +21,18 @@ import com.seiko.imageloader.model.ImageResult
 import dev.zt64.hyperion.ui.modifier.shimmer
 import dev.zt64.hyperion.ui.theme.HyperionTheme
 import dev.zt64.hyperion.ui.tooling.Preview
+import io.github.aakira.napier.Napier
 
 @Composable
 fun ShimmerImage(
     url: String,
     contentDescription: String?,
     modifier: Modifier = Modifier,
+    quality: FilterQuality = FilterQuality.None,
+    alignment: Alignment = Alignment.Center,
     contentScale: ContentScale = ContentScale.Fit,
+    alpha: Float = DefaultAlpha,
+    colorFilter: ColorFilter? = null
 ) {
     CompositionLocalProvider(
         LocalImageLoader provides rememberImageLoader()
@@ -32,29 +43,46 @@ fun ShimmerImage(
             }
         }
 
-        val action by rememberImageAction(request)
-        val painter = rememberImageActionPainter(action)
+        val imageAction by rememberImageAction(request)
+        val isLoading by remember { derivedStateOf { imageAction is ImageEvent } }
+        val isError by remember { derivedStateOf { imageAction is ImageResult.Error } }
+        val painter = rememberImageActionPainter(
+            action = imageAction,
+            filterQuality = quality,
+        )
 
-        Box(
-            contentAlignment = Alignment.Center
-        ) {
-            when (val current = action) {
-                is ImageEvent.Progress -> {
-                    CircularProgressIndicator()
-                }
+        val backgroundColor = MaterialTheme.colorScheme.surfaceColorAtElevation(
+            elevation = LocalAbsoluteTonalElevation.current + 3.dp
+        )
 
-                is ImageResult.Error -> {
-                    Text(current.error.message ?: "Error")
-                }
-
-                else -> Unit
+        DisposableEffect(isError) {
+            if (isError) {
+                Napier.e(
+                    tag = "Image",
+                    message = "Failed to load image",
+                    throwable = (imageAction as ImageResult.Error).error
+                )
             }
 
+            onDispose { }
+        }
+
+        Crossfade(
+            targetState = painter,
+            animationSpec = tween(300)
+        ) {
             Image(
-                modifier = modifier,
-                painter = painter,
+                painter = it,
                 contentDescription = contentDescription,
-                contentScale = contentScale
+                modifier = modifier.then(
+                    if (isLoading) Modifier.shimmer() else Modifier
+                ).then(
+                    if (isLoading || isError) Modifier.background(backgroundColor) else Modifier
+                ),
+                alignment = alignment,
+                contentScale = contentScale,
+                alpha = alpha,
+                colorFilter = colorFilter
             )
         }
     }
